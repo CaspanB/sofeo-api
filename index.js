7c6680536fefef9d5ac393d66d3e9ad18f1cb56b
CaspanB <berg.4.fhmdd@gmail.com>
2023-06-26 12:12:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const uuid = require('uuid')
 const cors = require('cors')
 const logger = require('morgan')
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const prisma = new PrismaClient()
 const app = express()
